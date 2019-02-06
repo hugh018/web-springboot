be4637db9c167aab418618c0dd2776475dacaaf1
hughhusong <hughhusong@aliyun.com>
2019-02-07 04:26:51 +0800

    Initial commit

---
+++ src/main/java/hugh/tech/learn/controller/testController.java
@@ -0,0 +1,22 @@
+package hugh.tech.learn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author: hugh
+ * Created on 2019/2/7
+ * func:
+ * params:
+ * link:
+ */
+
+@RestController
+@RequestMapping("/test")
+public class testController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello springboot";
+    }
+}
